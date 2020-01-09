8bf34610bf53ea09cf7d948d692b5ef4e110d758
Pierre Locus <pierrelocus@gmail.com>
2020-01-09 21:14:12 +0100

    Command line loop, will enhance later

--- ImageToText/script.py
+++ ImageToText/script.py
@@ -6,10 +6,27 @@ except ImportError:
     import Image
 import pytesseract
 import os
+import cmd
 
-filelist=os.listdir('.')
-nfile = open('documents.txt', 'w+')
-for fichier in filelist[:]:
-    if (fichier.endswith(".png")):
-        nfile.write(pytesseract.image_to_string(Image.open(fichier), lang='fra'))
+class ImageToText(cmd.Cmd):
+    def do_itt(self, line):
+        """Usage: itt <image> <file>
+        Will look for text on <image> and append it to <file>"""
+        image = line.split(' ')[0]
+        nfile = line.split(' ')[1]
+        try:
+            ofile = open(nfile, 'a+')
+            ofile.write(pytesseract.image_to_string(Image.open(image), lang='fra'))
+        except:
+            print("Error")
 
+    def do_ls(self, line):
+        """Lists files in current directory"""
+        print('\n'.join([i for i in os.listdir('.') if os.path.isfile(i)]))
+
+    def do_exit(self, line):
+        """Exits the program"""
+        return True
+
+if __name__ == '__main__':
+    ImageToText().cmdloop()
